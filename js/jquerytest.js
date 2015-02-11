ff652e961601fb1071d1a1dabf492da41df15e07
Jon Edwards <jon.edwards456@gmail.com>
2015-02-11 15:39:50 -0600

    added buttons to show and hide portfolio gallery

--- js/jquerytest.js
+++ js/jquerytest.js
@@ -1,11 +1,1 @@
-//SCRIPTS TO SHOW AND HIDE AN HTML ELEMENT
 
-var hiddenBox = $( "#bannerMessage" );
-$( "#buttonContainer button" ).on( "click", function( event ) {
-  hiddenBox.slideDown("slow");
-});
-
-
-$( "#buttonContainer2 button" ).on( "click", function( event ) {
-  hiddenBox.slideUp("slow");
-});
